0d3db356e221eb3c078feee73a93536260ab3bce
aaronsmedley <aaronsmedley44@gmail.com>
2015-02-14 14:59:58 -0500

    Removed Player Prefab added Rigidbody

---
+++ Assets/scripts/playerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate ()
+	{
+		if (Input.GetKey ("w")) {
+			rigidbody.AddRelativeForce(Vector3.forward * 10);
+		}
+	}
+}
