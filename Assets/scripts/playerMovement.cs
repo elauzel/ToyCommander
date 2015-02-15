b12ad6b38de0d38cc3a0f8b3fdc5529b5560cc0f
aaronsmedley <aaronsmedley44@gmail.com>
2015-02-15 15:54:46 -0500

    Physics Demo
    I created a little tower to demo the physics with. Player now interacts
    with other rigidbody's. Still need to add left and right rotation for
    player.

--- Assets/scripts/playerMovement.cs
+++ Assets/scripts/playerMovement.cs
@@ -10,13 +10,38 @@ public class playerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
 	void FixedUpdate ()
 	{
 		if (Input.GetKey ("w")) {
-			rigidbody.AddRelativeForce(Vector3.forward * 10);
+			rigidbody.AddRelativeForce(Vector3.right * 40);
 		}
+
+		if (Input.GetKey ("a")) {
+			rigidbody.AddRelativeForce(Vector3.forward * 20);
+		}
+
+		if (Input.GetKey ("s")) {
+			rigidbody.AddRelativeForce(Vector3.right * -40);
+		}
+
+		if (Input.GetKey ("d")) {
+			rigidbody.AddRelativeForce(Vector3.forward * -20);
+		}
+
+		if (Input.GetKey ("up")) {
+			rigidbody.AddRelativeForce(Vector3.up * 50);
+		}
+
+		if (Input.GetKey ("down")) {
+			rigidbody.AddRelativeForce(Vector3.up * -10);
+		}
+
+		if (Input.GetKey ("left")) {
+			rigidbody.transform.rotation = Quaternion.identity;
+		}
+
 	}
 }
