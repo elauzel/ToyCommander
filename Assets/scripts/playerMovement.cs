513ae3e0816f1d0ddb00147de15ea42f3ad8bb80
aaronsmedley <aaronsmedley44@gmail.com>
2015-02-16 13:15:12 -0500

    Added Left and Right Rotation to Player

--- Assets/scripts/playerMovement.cs
+++ Assets/scripts/playerMovement.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class playerMovement : MonoBehaviour {
 
+	public float rotationSpeed = 30f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -31,7 +33,7 @@ public class playerMovement : MonoBehaviour {
 			rigidbody.AddRelativeForce(Vector3.forward * -20);
 		}
 
-		if (Input.GetKey ("up")) {
+		if (Input.GetKey ("space")) {
 			rigidbody.AddRelativeForce(Vector3.up * 50);
 		}
 
@@ -42,6 +44,8 @@ public class playerMovement : MonoBehaviour {
 		if (Input.GetKey ("left")) {
 			rigidbody.transform.rotation = Quaternion.identity;
 		}
+
+		rigidbody.rotation = Quaternion.Euler(rigidbody.rotation.eulerAngles + new Vector3(0f, rotationSpeed*Input.GetAxis("Mouse X"), 0f));
 
 	}
 }
