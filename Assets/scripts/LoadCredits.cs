f51b6bbc2d1dc67c48b30644b0002199c8839893
JayMusick <compdevjay@gmail.com>
2015-03-02 22:40:45 -0500

    Commit this had better F***ing work
    redid scene 00, 01 and 07
    working ui buttons ans some scripts
    game menue and credits
    
    jay

---
+++ Assets/scripts/LoadCredits.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadCredits : MonoBehaviour {
+
+	public void loadCredits()
+	{
+		Application.LoadLevel ("07 - Credits");
+	}
+}
---
+++ Assets/scripts/LoadGameMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadGameMenu : MonoBehaviour {
+	
+	public void loadGameMenu()
+	{
+		Application.LoadLevel("01 - Game Menu");
+	}
+}
