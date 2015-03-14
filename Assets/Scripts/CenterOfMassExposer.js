c3dd9430395aeec3102936d80d36c42689493f3a
Jeremy Gilreath <elauzel@gmail.com>
2015-03-14 19:09:21 -0400

    A Weeks' Work
    More still to be done

---
+++ Assets/Scripts/CenterOfMassExposer.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+// Expose center of mass to allow it to be set from
+// the inspector.
+var com: Vector3;
+var rb: Rigidbody;
+
+function Start() {
+	rb = GetComponent.<Rigidbody>();
+	rb.centerOfMass = com;
+}
+
+function Update () {
+
+}
