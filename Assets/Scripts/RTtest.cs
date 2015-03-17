d9b41d61a0bb43fa22efc4f2486d3c3af0283a42
Jeremy Gilreath <elauzel@gmail.com>
2015-03-17 02:57:58 -0400

    Various Improvements
    Fixes and additions to all three scenes, such as the television in the
    bedroom broadcasting player action in real time

---
+++ Assets/Scripts/RTtest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RTtest : MonoBehaviour {
+	public float refresh = 0.01666666666666666666666666666667f;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating( "SimulateRenderTexure", 0f, refresh );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void SimulateRenderTexure() {		
+		renderer.material.mainTexture = RenderTextureFree.Capture();
+	}
+}
---
+++ Assets/Scripts/RenderTextureFree.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RenderTextureFree : MonoBehaviour {
+	/// <summary>
+	/// Simulate a RenderTexture. The process need a Texture.Apply, which takes a lot of time,
+	/// so don't use this every frames.
+	///
+	/// by Berenger Mantoue, www.berengermantoue.fr
+	///
+	/// </summary>
+	private static Texture2D tex2D;
+	private static Texture tex;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	// Return the entire screen in a texture
+	public static Texture Capture() {
+		return Capture( new Rect( 0f, 0f, Screen.width, Screen.height ), 0, 0 );
+	}
+	
+	// Return part of the screen in a texture.
+	public static Texture Capture(Rect captureZone, int destX, int destY) {
+		Texture2D result;
+		result = new Texture2D( Mathf.RoundToInt( captureZone.width ) + destX,
+		                       Mathf.RoundToInt( captureZone.height ) + destY,
+		                       TextureFormat.RGB24, false);
+		result.ReadPixels(captureZone, destX, destY, false);
+		
+		// That's the heavy part, it takes a lot of time.
+		result.Apply();
+		
+		return result;
+	}
+}
