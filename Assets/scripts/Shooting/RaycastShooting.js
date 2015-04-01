29f5c6de30decdf2f403c11b83c76428182f90af
aaronsmedley <aaronsmedley44@gmail.com>
2015-04-01 15:06:27 -0400

    More shooting additions

--- Assets/scripts/Shooting/RaycastShooting.js
+++ Assets/scripts/Shooting/RaycastShooting.js
@@ -1,6 +1,6 @@
 ﻿#pragma strict
 
-//var Effect : Transform;
+var Effect : Transform;
 var TheDamage = 100;
 
 private var lineTransform : Vector3;
@@ -13,14 +13,14 @@ function Start () {
 function Update () {
 
 	var hit :RaycastHit;
-	var ray : Ray = Camera.main.ScreenPointToRay(Vector2(Screen.width*0.5, 0));
+	var ray : Ray = Camera.main.ScreenPointToRay(Vector3(Screen.width*0.5, Screen.height*0.5, 0));
 	
 	if (Input.GetMouseButtonDown(0))
 	{
-		//var particleClone = Instantiate(Effect, hit.point, Quaternion.LookRotation(hit.normal));
+		var particleClone = Instantiate(Effect, hit.point, Quaternion.LookRotation(hit.normal));
 		//Destroy(particleClone.gameObject, 2);
 		hit.transform.SendMessage("ApplyDamage", TheDamage, SendMessageOptions.DontRequireReceiver);
-		Debug.DrawRay(startTransform, lineTransform, Color.red);
+		//Debug.DrawRay(startTransform, lineTransform, Color.red);
 	}
 }
 
