d1988c2b864f4090df24e240988ac30e9c4f8f27
Jeremy Gilreath <elauzel@gmail.com>
2015-04-07 09:06:14 -0400

    Truck Prefab
    Reimported some assets, fixed two scripts, added components to truck
    prefab

--- Assets/scripts/Networking/NewNetwork.cs
+++ Assets/scripts/Networking/NewNetwork.cs
@@ -2,15 +2,9 @@
 using System.Collections;
 
 public class NewNetwork : Photon.MonoBehaviour {
-<<<<<<< HEAD
 	GameObject player;
-	public float playerHealth = 100f;
-	public float startX, startY, startZ;
-=======
-	 GameObject player;
-
->>>>>>> origin/Aaron
-
+	
+	
 	// Use this for initialization
 	void Start () {
 		PhotonNetwork.ConnectUsingSettings("0.1");
@@ -18,47 +12,47 @@ public class NewNetwork : Photon.MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-		}
-
+		
+	}
+	
 	void OnGUI()
 	{
 		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
 	}
-
+	
 	void OnJoinedLobby()
 	{
 		PhotonNetwork.JoinRandomRoom();
 	}
-
+	
 	void OnPhotonRandomJoinFailed()
 	{
 		Debug.Log("Can't join random room!");
 		PhotonNetwork.CreateRoom(null);
 	} 
-
+	
 	void OnJoinedRoom()
 	{
-	
+		
 		//player = PhotonNetwork.Instantiate("Player - Truck", Vector3.zero, Quaternion.identity, 0);
 		//player = PhotonNetwork.Instantiate("Player - Truck", new Vector3(2f, 0.115f, 12f), Quaternion.identity, 0);
-		player = PhotonNetwork.Instantiate("Player - Truck", new Vector3(startX, startY, startZ), Quaternion.identity, 0);
-
+		player = PhotonNetwork.Instantiate("Player - Truck", new Vector3(2f, 1f, 12f), Quaternion.identity, 0);
+		
 		player.GetComponent<TruckMovement> ().enabled = true;
 		player.GetComponent<RaycastShooting1> ().enabled = true;
 		player.GetComponent<PlayerHealth1> ().enabled = true;
 		//((MonoBehaviour)player.GetComponent("RaycastShooting") ).enabled = true;
 		//((MonoBehaviour)player.GetComponent("PlayerHealth") ).enabled = true;
 		player.GetComponentInChildren<Camera>().enabled = true;
-
-}
-
+		
+	}
+	
 	public void OnPlayerDeath()
 	{
 		PhotonView photonView = PhotonView.Get(this);
 		photonView.RPC("Dead", PhotonTargets.All);
-
+		
 	}
 	
-
+	
 }
--- Assets/scripts/Player Settings/PlayerHealth.js
+++ Assets/scripts/Player Settings/PlayerHealth.js
@@ -23,8 +23,3 @@ function Dead()
 {
 	Destroy (gameObject);
 }
-
-
-
-
-
